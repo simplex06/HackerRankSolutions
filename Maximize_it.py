9212a8deb1f16050bcd39ce8986d01b57b760e67
Ozkan B <67653408+simplex06@users.noreply.github.com>
2020-08-28 13:35:52 +0200

    Add files via upload

---
+++ Maximize_it.py
@@ -0,0 +1,18 @@
+# HackerRank - Maximize it! - My solution
+
+import itertools
+
+k, m = map(int, input().strip().split(' '))
+
+lst = list()
+
+for i in range(k):
+    lst.append(input().split(' ')[1:])
+
+smax = 0
+for j in itertools.product(*lst):
+    modulo = sum([int(x)**2 for x in j]) % m
+    if modulo > smax:
+        smax = modulo
+
+print(smax)
